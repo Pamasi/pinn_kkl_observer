fixed normalization and obs dynamics

neural_network.py
@@ -7,18 +7,30 @@ class FCN(nn.Module):
     """
     FullyConnectedNeural Network
     """
-    def __init__(self, num_hidden, hidden_size, in_size, out_size, activation, normalizer=None, p_drop=0.2):
+    def __init__(self, num_hidden, hidden_size, in_size, out_size, 
+                 activation, normalizer=None, p_drop=0.2):
         super().__init__()
         self.layers = nn.ModuleList()
         self.dropout = nn.Dropout(p=p_drop)
         self.activation = activation
         self.normalizer = normalizer
-        self.mode = 'normal'
+        self.mode = None
+ 
         current_dim = in_size
         for _ in range(num_hidden):
             self.layers.append(nn.Linear(current_dim, hidden_size))
             current_dim = hidden_size
         self.layers.append(nn.Linear(current_dim, out_size))
+
+
+
+    @property
+    def mode(self):
+        return self.normalizer.mode 
+    
+    @mode.setter
+    def mode(self, value):
+        self.normalizer.mode = value
 
     def forward(self, x):
         """
@@ -26,7 +38,7 @@ class FCN(nn.Module):
         If a normalizer object is passed to the class, the network will normalize the input and denormalize the output.
         """
         # Normalize input here
-        if self.normalizer != None:
+        if self.normalizer is not None:
             x = self.normalizer.Normalize(x, self.mode).float()
  
         for layer in self.layers[:-1]:
@@ -36,7 +48,7 @@ class FCN(nn.Module):
         x = self.dropout(self.layers[-1](x))
 
         # Denormalize output here
-        if self.normalizer != None:
+        if self.normalizer is not None:
             x = self.normalizer.Denormalize(x, self.mode).float()
         return x
 
@@ -52,7 +64,7 @@ class EncoderDecoder(nn.Module):
                            z_size, activation, normalizer)
         self.decoder = FCN(num_hidden, hidden_size, z_size,
                            x_size, activation, normalizer)
-        self.mode = 'normal'
+        self.mode = None
 
     def forward(self, x) -> Tuple[torch.Tensor]:
         self.encoder.mode = self.mode

train.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+import math
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -291,7 +292,7 @@ def experiment(args: argparse.Namespace):
     # split traning and validation following literature
     t_init_train = 0
     t_end_train = t_init_train + args.t_sim/2
-
+    # TODO increase trajectory time
     # validation simulation time
     t_init_val = args.t_sim/2
     t_end_val = args.t_sim
@@ -307,7 +308,16 @@ def experiment(args: argparse.Namespace):
         raise ValueError('System  is still not implemented')
 
     # A and B matricies
+
+    # real matrix
     A = np.array(np.diag(-np.arange(1, system.z_size + 1, 1)))
+
+    # using paper https://proceedings.mlr.press/v242/peralez24a/peralez24a.pdf
+    # alpha = 0.5
+    # omega = 0.5
+    # asyn_dynamic = np.asarray([math.cos(omega), -math.sin(omega), math.sin(omega), math.cos(omega)]).reshape(2,2)/(1+np.exp(alpha))
+    # complex conjugate
+    #A = np.block([  [asyn_dynamic, np.zeros((2, 2))], [ np.zeros((2, 2)), asyn_dynamic ]])
 
     B = np.ones([system.z_size, system.y_size])
 

utils/dataset.py
@@ -97,7 +97,7 @@ class DataSet(torch.utils.data.Dataset):
             self.ic_normal = self.train_data[4]
             # -----------------------------------------------------------------
 
-            # ----------------------- Physics loss data -----------------------
+            # -------self.mea---------------- Physics loss data -----------------------
             self.train_data_ph = self.generate_data(seed=seed)
             # Total number of samples
             self.data_length_ph = self.train_data_ph[0].shape[0] * \